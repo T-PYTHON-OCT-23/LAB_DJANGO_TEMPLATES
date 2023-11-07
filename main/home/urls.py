f2717134596ce4bbba1b16a6b1df64f08969e26d
AlbandariAljabri <Albandarialjabri@gmail.com>
2023-11-08 02:16:25 +0300

    done

---
+++ main/home/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from . import views
+
+app_name = "home"
+
+#path("main/home/", views.page_view, name="page_view")
+urlpatterns = [ path("", views.page_view, name="page_view"),
+    path("today/", views.today_view, name="today_view"),
+    path("fav_games/", views.games_view, name="games_view"),
+    path("password/", views.password_view, name="password_view"),
+]
---
+++ main/home/views.py
@@ -0,0 +1,35 @@
+from django.shortcuts import render
+from django.http import HttpRequest, HttpResponse
+from datetime import *
+from django.shortcuts import render
+from django.utils.crypto import get_random_string
+
+# Create your views here.
+
+
+# Create your views here.
+def page_view(request :HttpRequest):
+      
+      return render(request, "home/home.html" )
+
+
+def today_view(request : HttpRequest):
+   contaxt = { "today" : datetime.today()   
+   }
+   return render(request, "home/today.html" , contaxt )
+
+
+def password_view(request :HttpRequest):
+   
+   dict= { "password" : get_random_string(8)}
+   return render(request, "home/password.html" , dict )
+
+
+
+def games_view(request :HttpRequest):
+        return render(request, "home/fav_games.html"  ) 
+
+'''   fav_games =["Tarazan" , "crach" , "driver" , "fefa" ]
+   dict = {"games" : fav_games}'''
+ 
+
