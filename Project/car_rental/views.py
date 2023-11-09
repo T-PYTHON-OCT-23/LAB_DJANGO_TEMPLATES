58219be5e79af5600d5f91f8874dbb890d6772fd
naif <iimnaif@gmail.com>
2023-11-09 13:21:49 +0300

    h

---
+++ Project/car_rental/views.py
@@ -0,0 +1,34 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+import time
+from django.utils.crypto import get_random_string
+
+# Create your views here.
+
+def index_viwe(request:HttpResponse):
+
+    return render(request , 'car_rental/index.html')
+
+def today_viwe(request:HttpResponse):
+    time_now = time.ctime(time.time())
+
+    context ={
+        "today" : time_now 
+    }
+    return render(request,'car_rental/today.html',context)
+
+def random_password(request:HttpResponse):
+    password=get_random_string(12)
+    context ={
+        "random_password" : password
+    }
+    return render(request,'car_rental/random_password.html',context)
+
+def favs_games(request:HttpResponse):
+    list_favs_games=["X & O","Pepsi man","Fifa EA"]
+    context = {
+        "games" : list_favs_games
+
+    }
+
+    return render(request,'car_rental/favs_games.html',context)
--- Project/main/views.py
+++
@@ -1,8 +0,0 @@
-from django.shortcuts import render
-from django.http import HttpRequest, HttpResponse
-
-# Create your views here.
-#view in django
-def index_view(request:HttpRequest):
-
-    return render(request, 'main/home.html')
