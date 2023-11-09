46278da2a68a76e4895369a5b191fc14fc619519
khamis_dossary <khamis.dossary@gmail.com>
2023-11-09 15:07:31 +0300

    done

--- myProject/main/views.py
+++ myProject/main/views.py
@@ -16,7 +16,12 @@ def random_password(request):
 
 def favorite_games(request):
     favorite_games_list = [
-        {'title': 'Overwatch 2', 'image_url': 'https://fs-prod-cdn.nintendo-europe.com/media/images/10_share_images/games_15/nintendo_switch_download_software_1/2x1_NSwitchDS_Overwatch2_Season6.png'},
-        {'title': 'Fortnite', 'image_url': 'https://cdn2.unrealengine.com/social-image-chapter4-s3-3840x2160-d35912cc25ad.jpg'},
+        {
+        'title': 'Overwatch 2', 
+        'image': '/static/images/overwatch.png'
+        },
+        {'title': 'Fortnite',
+        'image': '/static/images/fortnite.jpeg'
+        },
     ]
     return render(request, 'main/favorite_games.html', {'favorite_games_list': favorite_games_list})
