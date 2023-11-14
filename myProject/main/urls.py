7339015a1a42243ff915cac9c3dfc8eadb5b0170
khamis_dossary <khamis.dossary@gmail.com>
2023-11-14 07:06:55 +0300

    done

--- myProject/main/urls.py
+++ myProject/main/urls.py
@@ -6,4 +6,10 @@ urlpatterns = [
     path('today/', views.today, name='today'),
     path('random/password/', views.random_password, name='random_password'),
     path('favs/games/', views.favorite_games, name='favorite_games'),
-]
+    path('about/',views.about, name='about'),
+    path("big/view/", views.big_view, name="big_view"),
+    path('small/view/', views.small_view, name="small_view"),
+    path('big/about/', views.big_about, name="big_about"),
+    path('small/about/', views.small_about, name="small_about"),
+    path('reset/', views.reset, name="reset")
+    ]
--- myProject/main/views.py
+++ myProject/main/views.py
@@ -1,7 +1,7 @@
-from django.shortcuts import render
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.utils.crypto import get_random_string
 from datetime import date
+from django.http import HttpResponse, HttpResponseRedirect
 
 def home(request):
     return render(request, 'main/home.html')
@@ -25,3 +25,38 @@ def favorite_games(request):
         },
     ]
     return render(request, 'main/favorite_games.html', {'favorite_games_list': favorite_games_list})
+
+
+def about(request):
+    return render(request,'main/about.html')
+
+def big_view(request):
+
+    response = redirect("/")
+    response.set_cookie('size', 'biggest')
+    return response
+
+def small_view(request):
+
+    response = redirect("home")
+    response.set_cookie('size', 'smallest')
+    return response
+
+def big_about(request):
+
+    response = redirect("about")
+    response.set_cookie('size_about', 'biggest')
+    return response
+
+def small_about(request):
+
+    response = redirect("about")
+    response.set_cookie('size_about', 'smallest')
+    return response
+
+def reset(request):
+    response= redirect("home")
+    response.delete_cookie("size")
+    response.delete_cookie("size_about")
+
+    return response 
